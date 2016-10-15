010e9d74dd3a843d3e0e9bd4f9571991a712d273
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2016-10-15 14:52:17 +0200

    libparted: Add support for atari partition tables

--- libparted/tests/common.c
+++ libparted/tests/common.c
@@ -68,6 +68,7 @@ _implemented_disk_label (const char *label)
 {
         /* FIXME: these have minor problems, so skip them, temporarily.  */
         if (STREQ (label, "amiga")) return 0;
+        if (STREQ (label, "atari")) return 0;
 
         /* Not implemented yet */
         if (STREQ (label, "aix")) return 0;
